fe29218e119e536b310d391cc6c1da7b1a67e53f
r1f <33310611+r1f@users.noreply.github.com>
2021-12-13 13:48:33 +0300

    Update 7.sql

--- queries/7.sql
+++ queries/7.sql
@@ -1,7 +1,3 @@
-/*
-7. Предоставить возможность добавления и изменения информации о продавцах и покупателях.
-*/
-
 UPDATE Person
 SET FirstName_Per = 'Rumas', LastName_Per = 'Arngel'
 WHERE ID_Per=1;
