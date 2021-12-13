6e28f1f8ebfaddd25b71e1c30ace1f71fe71764b
r1f <33310611+r1f@users.noreply.github.com>
2021-12-13 13:47:44 +0300

    Update 3.sql

--- queries/3.sql
+++ queries/3.sql
@@ -1,7 +1,3 @@
-/*
-3. Для указанного интервала дат вывести список продавцов в порядке убывания общей суммы, полученной ими от продажи предметов этот промежуток времени;
-*/
-
 SELECT DISTINCT P.ID_Per, P.FirstName_Per, P.LastName_Per, 
 (SELECT SUM(LOA.Last_Prise) FROM LotOnAuctions LOA
 		INNER JOIN Auctions A ON LOA.ID_Auct=A.ID_Auct
