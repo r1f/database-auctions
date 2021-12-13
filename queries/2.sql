2ed6e251ad6de3335eb083d1f41167ba7891b969
r1f <33310611+r1f@users.noreply.github.com>
2021-12-13 13:47:24 +0300

    Update 2.sql

--- queries/2.sql
+++ queries/2.sql
@@ -1,7 +1,3 @@
-/*
-2. Вывести список аукционов с указанием отсортированных по величине суммарных доходов от продажи.
-*/
-
 SELECT A.ID_Auct, A.Specifics_Auct, 
 	(
 	SELECT SUM(LOA.Last_Prise) FROM LotOnAuctions LOA 
